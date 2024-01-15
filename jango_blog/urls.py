c50fef9017737c6ca808ed20105ae53d45eddb53
Abir <abir4dev@gmail.com>
2024-01-15 23:06:09 +0600

    deploy

--- jango_blog/urls.py
+++ jango_blog/urls.py
@@ -31,6 +31,4 @@ urlpatterns = [
     path('', include('blog.urls'))
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-# Remove this
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT_IMG)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
