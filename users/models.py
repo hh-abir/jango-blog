e3235adb02577ae805eaf3a70cabf698e96456d6
Abir <abir4dev@gmail.com>
2024-01-16 01:47:43 +0600

    user profile

--- users/models.py
+++ users/models.py
@@ -1,20 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
-from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default= 'default.jpg', upload_to='profile_pics')
+    image = models.CharField(max_length=255, default='https://cdn-icons-png.flaticon.com/512/21/21294.png')
 
     def __str__(self):
         return f"{self.user.username} Profile "
-    
-    def save(self, *args, **kwargs):
-        super().save()
 
-        img = Image.open(self.image.path)
-
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
